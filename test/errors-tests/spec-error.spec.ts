88297c7d23ab2d33d5a2346bb333d19d53736b77
chris_dibbern <chrisdibbern@gmail.com>
2018-04-18 23:20:04 -0500

    Bumping tsnode version to 6.0.0. Fixing spec-error test.

--- test/errors-tests/spec-error.spec.ts
+++ test/errors-tests/spec-error.spec.ts
@@ -13,17 +13,17 @@ export class SpecErrorTests {
     @Test()
     public constructor_buildsCorrectAssertPath() {
         let node = new RootNode("Assert", "object");
-        let child = new FluentMatcherBase({}, null);
+        let child = new FluentMatcherBase({}, null, false);
         child.parent = node;
         child.name = "equals";
         child.details = "object";
         node = child;
-        child = new FluentMatcherBase({}, null);
+        child = new FluentMatcherBase({}, null, false);
         child.parent = node;
         child.name = "that";
         child.details = undefined;        
         node = child;
-        child = new FluentMatcherBase({}, null);
+        child = new FluentMatcherBase({}, null, false);
         child.parent = node;
         child.name = "has";
         child.details = undefined;
